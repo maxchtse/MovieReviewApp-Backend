46cf18e283b8150c9e105da57d454a05a3a23941
maxchtse <tsechunghei97@outlook.com>
2024-01-27 22:03:51 +0800

    Connect to MongoDB, Create Movies and Review Endpoints

---
+++ src/main/java/dev/maxchtse/movies/MovieService.java
@@ -0,0 +1,20 @@
+package dev.maxchtse.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MovieService {
+    @Autowired
+    private MovieRepository movieRepository;
+    public List<Movie> allMovies() {
+        return movieRepository.findAll();
+    }
+
+    public Movie singleMovie(ObjectId id) {
+        return movieRepository.findBy(id);
+    }
+}
